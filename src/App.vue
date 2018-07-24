49018c04204cc4794497446e1fc2f1cc0ab87203
Nesar Ahmed Nishad <rockstar.nishad@gmail.com>
2018-07-24 13:32:54 +0600

    Nishad

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+	<div id="app">
+    <h1 align="center">Welcome.....</h1>
+    <hr>
+    <textarea rows="10" v-model="text" class="textarea"></textarea>
+    <br>
+		<text-reader @load="text = $event"></text-reader>
+	</div>
+</template>
+
+<script>
+import TextReader from "./TextReader";
+
+export default {
+  name: "app",
+  data: () => ({ text: "" }),
+  components: {
+    TextReader
+  }
+};
+</script>
+<style type="text/css">
+  .textarea{
+    background-color: #FFCECE;
+    border:5px;
+  }
+</style>
